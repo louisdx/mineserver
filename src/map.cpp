8d25ce3b6c327b6c539fa413d83d65ac9ff0a9d5
Mitchell Marx <mitchell.marx@gmail.com>
2011-05-04 22:03:01 -0700

    fix from louisdx
    Fixed a bug in the bugfix
    louisdx (author)
    commit  ecbfd781c579cc2757c8
    tree    f4d73a3855bb95b76e53
    parent  24fe85a2b3f1dbfb5a06

--- src/map.cpp
+++ src/map.cpp
@@ -251,7 +251,7 @@ void Map::init(int number)
   const char* key = "map.storage.nbt.directories"; // Prefix for worlds config
   std::auto_ptr< std::list<std::string> > tmp(Mineserver::get()->config()->mData(key)->keys());
 
-  if (number < tmp->size())
+  if (number < int(tmp->size()))
   {
     std::list<std::string>::const_iterator it = tmp->begin();
     std::advance(it, number);
